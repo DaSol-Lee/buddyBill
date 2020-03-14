07e102f9511421bb88f4cdc9ee3979660fd19d84
Da Sol Lee <dasl1@uci.edu>
2020-03-14 16:23:59 -0700

    First Screen

---
+++ buddyBill/buddyBill/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  buddyBill
+//
+//  Created by Da Sol Lee on 3/14/20.
+//  Copyright © 2020 dasol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
